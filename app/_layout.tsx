816bc39b73d773f8f9d678daa8d147043d7ed9f3
Ian Pflieger <ian.pflieger@scsstudent.org>
2026-02-23 14:50:36 -0800

    feat: complete video 03

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,30 @@
-import { Stack } from "expo-router";
+import { Stack, Tabs } from "expo-router";
+import { Entypo, Feather } from "@expo/vector-icons";
+import { theme } from "../theme";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}>
+      <Tabs.Screen
         name="index"
         options={{
           title: "Home",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="leaf" size={size} color={color} />
+          ),
         }}
       />
-    </Stack>
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Home",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
