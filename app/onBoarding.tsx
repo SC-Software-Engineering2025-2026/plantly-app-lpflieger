0fff5ed1b385f1a9610aa5e5ba0a061dea8f7c2d
Ian Pflieger <ian.pflieger@scsstudent.org>
2026-02-27 13:49:16 -0800

    feat: complete video 08

--- app/onBoarding.tsx
+++ app/onBoarding.tsx
@@ -2,7 +2,8 @@ import { PlantlyButton } from "@/components/PlantlyButton";
 import { useUserStore } from "@/store/userStore";
 import { theme } from "@/theme";
 import { useRouter } from "expo-router";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, Text, Button, StatusBar } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function OnboardingScreen() {
   const router = useRouter();
@@ -12,9 +13,15 @@ export default function OnboardingScreen() {
     router.replace("/");
   };
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      colors={[theme.colorGreen, theme.colorAppleGreen, theme.colorLimeGreen]}
+      style={styles.container}
+    >
+      <StatusBar barStyle="light-content" />
       <PlantlyButton title="Let me in!" onPress={handlePress} />
-    </View>
+    </LinearGradient>
   );
 }
 
--- theme.ts
+++ theme.ts
@@ -2,4 +2,6 @@ export const theme = {
   colorGreen: "#29b365",
   colorWhite: "#fff",
   colorLeafyGreen: "#206a42",
+  colorAppleGreen: "#29b365",
+  colorLimeGreen: "#d0e57e",
 };
